8ba316dca2503dc44b448eb23c63d1cc6562f98b
Daniel Sena <daniels_araujo@outlook.com.br>
2026-01-15 21:59:20 -0300

    Cap. 08 - Preparando um ambiente de testes

--- ntalk/app.js
+++ ntalk/app.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
 const config = require('./config');
 const error = require('./middlewares/error');
 
-mongoose.connect('mongodb://localhost:27017/ntalk');
+mongoose.connect(config.mongoDBURL);
 
 const app = express();
 const server = http.Server(app);
@@ -28,6 +28,7 @@ app.use(expressSession({
   name: config.sessionKey,
   secret: config.sessionSecret
 }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -51,7 +52,7 @@ io.use((socket, next) => {
   });
 });
 
-consign({})
+consign({ verbose: false })
   .include('models')
   .then('controllers')
   .then('routes')
@@ -62,3 +63,5 @@ app.use(error.notFound);
 app.use(error.serverError);
 
 server.listen(3000, () => console.log('Ntalk no ar.'));
+
+module.exports = app;
